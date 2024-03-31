b126855a9605fdfa05e880d25ccf317fb4fd1c13
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-03-30 22:53:19 -0300

    ADD, styles

--- components/ui/ImgBanner.tsx
+++ components/ui/ImgBanner.tsx
@@ -8,8 +8,8 @@ interface ImgBannerProps {
 
 const ImgBanner: React.FC<ImgBannerProps> = ({ src, alt }) => {
   return (
-    <div className="w-full max-w-screen-xl">
-      <Image src={src} alt={alt} width={1200} height={300} style={{ width: "100%", height: "auto" }} />
+    <div className="w-full">
+      <Image src={src} alt={alt} layout="responsive" width={1200} height={300} />
     </div>
   );
 };
