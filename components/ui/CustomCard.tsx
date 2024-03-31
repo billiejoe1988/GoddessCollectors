cef9cb6c29e52189db2ac59742220984cdea9c28
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-03-31 02:20:32 -0300

    ADD, fix build apos

--- components/ui/CustomCard.tsx
+++ components/ui/CustomCard.tsx
@@ -8,31 +8,54 @@ export default function CustomCard() {
       <div className="max-w-[1200px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
         <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
           <Link href="/pages/Sculpture">
-            <Image
-              removeWrapper
-              alt="Card background"
-              className="z-0 w-full h-full object-cover"
-              src="/bzSD.avif"
-            />
+            <div className="overflow-hidden">
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src="/bzSD.avif"
+              />
+            </div>
           </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
             <p className="text-tiny text-white/60 uppercase font-bold">Next Arrived</p>
             <h4 className="text-white font-medium text-large">Guntz 1/6</h4>
           </CardHeader>
           <CardFooter className="absolute bottom-1 left-1 right-1 z-10 p-4 bg-black bg-opacity-50 text-white text-small" style={{ backdropFilter: "blur(8px)" }}>
-            Discover the new Guntz 1/6 statue, now available for pre-order! Limited edition, don't miss out!
+            Discover the new Guntz 1/6 statue, now available for pre-order! Limited edition, don&apos;t miss out!
           </CardFooter>
         </Card>
         <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
           <Link href="/pages/Sculpture">
-            <Image
-              removeWrapper
-              alt="Card background"
-              className="z-0 w-full h-full object-cover"
-              src="/hqSD.avif"
-            />
+            <div className="overflow-hidden">
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src="/jkSD.avif"
+              />
+            </div>
           </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
+            <p className="text-tiny text-white/60 uppercase font-bold">New Arrival</p>
+            <h4 className="text-white font-medium text-large">Joker Prime 1 1/4</h4>
+          </CardHeader>
+          <CardFooter className="absolute bottom-1 left-1 right-1 z-10 p-4 bg-black bg-opacity-50 text-white text-small" style={{ backdropFilter: "blur(8px)" }}>
+            Introducing the breathtaking Joker Prime 1 1/4 statue! Get ready to experience the madness!
+          </CardFooter>
+        </Card>
+        <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
+          <Link href="/pages/Sculpture">
+            <div className="overflow-hidden">
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src="/hqSD.avif"
+              />
+            </div>
+          </Link>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
             <p className="text-tiny text-white/60 uppercase font-bold">Coming Soon</p>
             <h4 className="text-white font-medium text-large">Harley Queen Sideshow 1/6</h4>
           </CardHeader>
@@ -42,19 +65,21 @@ export default function CustomCard() {
         </Card>
         <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
           <Link href="/pages/Sculpture">
-            <Image
-              removeWrapper
-              alt="Card background"
-              className="z-0 w-full h-full object-cover"
-              src="/jkSD.avif"
-            />
+            <div className="overflow-hidden">
+              <Image
+                removeWrapper
+                alt="Card background"
+                className="z-0 w-full h-full object-cover"
+                src="/srSD.avif"
+              />
+            </div>
           </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
-            <p className="text-tiny text-white/60 uppercase font-bold">New Arrival</p>
-            <h4 className="text-white font-medium text-large">Joker Prime 1 1/4</h4>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
+            <p className="text-tiny text-white/60 uppercase font-bold">Limited Edition</p>
+            <h4 className="text-white font-medium text-large">Saruman Wepa 1/4</h4>
           </CardHeader>
           <CardFooter className="absolute bottom-1 left-1 right-1 z-10 p-4 bg-black bg-opacity-50 text-white text-small" style={{ backdropFilter: "blur(8px)" }}>
-            Introducing the breathtaking Joker Prime 1 1/4 statue! Get ready to experience the madness!
+            Don&apos;t miss out on the limited edition Saruman Wepa 1/4 statue! A collector&apos;s dream, order now!
           </CardFooter>
         </Card>
         <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
@@ -66,7 +91,7 @@ export default function CustomCard() {
               src="/grSD.avif"
             />
           </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
             <p className="text-tiny text-white/60 uppercase font-bold">Exclusive Edition</p>
             <h4 className="text-white font-medium text-large">Grogu 1/10 Iron Studios</h4>
           </CardHeader>
@@ -83,29 +108,12 @@ export default function CustomCard() {
               src="/lolSD.avif"
             />
           </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
+          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col &excl;items-start" style={{ backdropFilter: "blur(8px)" }}>
             <p className="text-tiny text-white/60 uppercase font-bold">Coming Soon</p>
             <h4 className="text-white font-medium text-large">Ahri League of Legend 1/4</h4>
           </CardHeader>
           <CardFooter className="absolute bottom-1 left-1 right-1 z-10 p-4 bg-black bg-opacity-50 text-white text-small" style={{ backdropFilter: "blur(8px)" }}>
             Prepare to embark on an epic journey with the Ahri League of Legend 1/4 statue! Coming soon, stay tuned!
-          </CardFooter>
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[500px] p-4 cursor-pointer relative" data-hover="true" data-focus="true" style={{ border: "solid 2px transparent" }}>
-          <Link href="/pages/Sculpture">
-            <Image
-              removeWrapper
-              alt="Card background"
-              className="z-0 w-full h-full object-cover"
-              src="/srSD.avif"
-            />
-          </Link>
-          <CardHeader className="absolute top-1 left-1 right-1 z-10 flex-col !items-start" style={{ backdropFilter: "blur(8px)" }}>
-            <p className="text-tiny text-white/60 uppercase font-bold">Limited Edition</p>
-            <h4 className="text-white font-medium text-large">Saruman Wepa 1/4</h4>
-          </CardHeader>
-          <CardFooter className="absolute bottom-1 left-1 right-1 z-10 p-4 bg-black bg-opacity-50 text-white text-small" style={{ backdropFilter: "blur(8px)" }}>
-            Don't miss out on the limited edition Saruman Wepa 1/4 statue! A collector's dream, order now!
           </CardFooter>
         </Card>
       </div>
