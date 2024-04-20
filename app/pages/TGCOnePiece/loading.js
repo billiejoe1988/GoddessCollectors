794ee6e5a9c4b20737e5e060bfdf3ee9b280c832
MauriArbelaiz <mauricio.arbelaiz@gmail.com>
2024-04-20 16:48:13 -0300

    ADD, Route Handler, revalidateTag y Data Fetching

--- app/pages/TGCOnePiece/loading.js
+++ app/pages/TGCOnePiece/loading.js
@@ -3,6 +3,8 @@ import { Spinner } from "@nextui-org/react";
 
 export default function LoadingSpinner() {
   return (
-    <Spinner label="Loading..." color="warning" />
+    <div className="w-full h-full min-h-screen flex justify-center items-center">
+        <Spinner label="Loading..." color="warning" />
+    </div>
   );
 }
